18a023d9c36bf108cd654c8b48d57275179e94f4
alk831 <adam54111@gmail.com>
2019-07-26 18:34:20 +0200

    add desing lib

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { DatabaseProvider } from './context/database';
+import { Home } from './pages/Home';
+import { mapping, light as lightTheme } from '@eva-design/eva';
+import { ApplicationProvider } from 'react-native-ui-kitten';
+
+export const App = () => {
+  return (
+    <DatabaseProvider>
+      <ApplicationProvider
+        mapping={mapping}
+        theme={lightTheme}
+      >
+        <Home />
+      </ApplicationProvider>
+    </DatabaseProvider>
+  );
+};
---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Layout, Text } from 'react-native-ui-kitten';
+
+export const Home = () => (
+  <Layout style={{flex: 1}}>
+    <Text category="h3">Home</Text>
+  </Layout>
+);
