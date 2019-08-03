d4c1026bb10d396ea03c96018a9bbbb50a484328
alk831 <adam54111@gmail.com>
2019-08-03 12:07:58 +0200

    native: add react-native-svg lib

--- metro.config.js
+++ metro.config.js
@@ -5,13 +5,20 @@
  * @format
  */
 
+const { sourceExts, assetExts } = require('metro-config/src/defaults/defaults');
+
 module.exports = {
   transformer: {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
         inlineRequires: false,
+        babelTransformerPath: require.resolve('react-native-svg-transformer')
       },
+      resolver: {
+        assetExts: assetExts.filter(ext => ext !== "svg"),
+        sourceExts: [...sourceExts, "svg"]
+      }
     }),
   },
 };
