148a1685757326e409770d20d0cebbd8cc92cda1
alk831 <adam54111@gmail.com>
2019-08-07 17:02:17 +0200

    throw if userId is not initialized

---
+++ src/common/error.ts
@@ -0,0 +1,1 @@
+export class UserIdCannotBeNullError extends Error {}
--- src/common/hooks/useUserId.ts
+++ src/common/hooks/useUserId.ts
@@ -1,10 +1,17 @@
 import { useSelector } from 'react-redux';
 import { AppState } from '../../store';
 import { UserId } from '../../types';
+import { UserIdCannotBeNullError } from '../error';
 
-export function useUserId(): UserId | null {
+export function useUserId(): UserId {
   const userId = useSelector<AppState, UserId | null>(
     state => state.user.data && state.user.data.id
   );
+  if (userId === null) {
+    throw new UserIdCannotBeNullError(
+      'User id cannot be null. You must have made something wrong, ' +
+      'or userId hook has been called before app initialization.'
+    );
+  }
   return userId;
 }
