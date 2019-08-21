bc18f245def0b1d17681cb164ef8cdec0b8bfb80
alk831 <adam54111@gmail.com>
2019-08-21 12:05:05 +0200

    move database snapshots to upper level

--- src/database/index.spec.ts
+++ src/database/index.spec.ts
@@ -1,10 +1,10 @@
-import { getDatabaseSchema } from '../utils/getSchema';
+import { getDatabaseSchema } from './utils/getSchema';
 import sqlFormatter from 'sql-formatter';
 
 test('entity sql matches snapshot', async () => {
-  const tableSchema = await getDatabaseSchema();
+  const schema = await getDatabaseSchema();
 
-  for (const { sql, tbl_name } of tableSchema) {
+  for (const { sql, tbl_name } of schema) {
     if (tbl_name !== 'sqlite_sequence') {
       const prettySql = sqlFormatter.format(sql);
       expect(prettySql).toMatchSnapshot(tbl_name);
