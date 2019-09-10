85c8fe61c35e909f7fdf15291cd92afa7c9f4947
alk831 <adam54111@gmail.com>
2019-09-10 11:35:15 +0200

    cleanup

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,6 @@
 import {
   MACRO_ELEMENTS,
   BASE_MACRO_ELEMENTS,
-  USER_ID_UNSYNCED,
   PORTION_TYPES,
   UNIT_TYPES,
 } from './common/consts';
@@ -9,8 +8,6 @@ import * as Screens from './screens';
 
 export type UnitType = typeof UNIT_TYPES[number];
 export type ProductUnit = 'g' | 'ml';
-
-export type ArrayElement<ArrayType extends readonly unknown[]> = ArrayType[number];
 
 export type BaseMacroElement = typeof BASE_MACRO_ELEMENTS[number];
 export type MacroElement = typeof MACRO_ELEMENTS[number];
@@ -33,11 +30,6 @@ export type MealId = MealIdBrand & number;
 
 enum UserIdBrand {};
 export type UserId = UserIdBrand & number;
-
-// proposal type for userId of user
-// that has not been synced/registerd in online mode
-type UserIdUnsynced = typeof USER_ID_UNSYNCED
-type _UserId = UserId | UserIdUnsynced;
 
 export type PortionType = typeof PORTION_TYPES[number];
 
