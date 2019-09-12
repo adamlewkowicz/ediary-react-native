bbe05a38eb8c3811924574c365f7d2a79446ffc9
alk831 <adam54111@gmail.com>
2019-09-12 19:42:39 +0200

    make url unique per productId

--- src/database/entities/ProductImage.ts
+++ src/database/entities/ProductImage.ts
@@ -4,18 +4,20 @@ import {
   ManyToOne,
   Column,
   JoinColumn,
+  Unique,
 } from 'typeorm';
 import { Product } from './Product';
 import { GenericEntity } from '../generics/GenericEntity';
 import { ProductId } from '../../types';
 
+@Unique(['url', 'productId'])
 @Entity('product_images')
 export class ProductImage extends GenericEntity {
 
   @PrimaryGeneratedColumn()
   id!: number;
 
-  @Column({ unique: true })
+  @Column()
   url!: string;
 
   @Column()
