f9e584ff15621f181e4ee012914e2d6c5f9b5c69
alk831 <adam54111@gmail.com>
2019-09-12 19:46:50 +0200

    merge and update migrations

--- src/database/migrations.ts
+++ src/database/migrations.ts
@@ -1,3 +1,1 @@
-export * from './migrations/1568116851233-Initial';
-export * from './migrations/1568286894257-UpdateProductIdColumn';
-export * from './migrations/1568293558949-UpdateUrlColumn';
+export * from './migrations/1568310202209-Initial';
--- src/database/migrations/1568310202209-Initial.ts
+++ src/database/migrations/1568310202209-Initial.ts
@@ -1,6 +1,6 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class Initial1568116851233 implements MigrationInterface {
+export class Initial1568310202209 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
         await queryRunner.query(`CREATE TABLE "profile" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "weight" tinyint NOT NULL, "height" tinyint NOT NULL, "age" tinyint NOT NULL, "male" boolean NOT NULL, "weightGoal" text NOT NULL, "userId" integer NOT NULL, "macroNeedsCarbs" decimal(6,2) NOT NULL DEFAULT (0), "macroNeedsProts" decimal(6,2) NOT NULL DEFAULT (0), "macroNeedsFats" decimal(6,2) NOT NULL DEFAULT (0), "macroNeedsKcal" decimal(6,2) NOT NULL DEFAULT (0), CONSTRAINT "REL_a24972ebd73b106250713dcddd" UNIQUE ("userId"), CONSTRAINT "CHK_e13c7952dc8655e48c8f83608a" CHECK ("weightGoal" IN ('decrease','maintain','increase')))`);
@@ -8,7 +8,7 @@ export class Initial1568116851233 implements MigrationInterface {
         await queryRunner.query(`CREATE TABLE "meal" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar NOT NULL, "date" text NOT NULL DEFAULT (CURRENT_TIMESTAMP), "userId" integer, "macroCarbs" decimal(6,2) NOT NULL DEFAULT (0), "macroProts" decimal(6,2) NOT NULL DEFAULT (0), "macroFats" decimal(6,2) NOT NULL DEFAULT (0), "macroKcal" decimal(6,2) NOT NULL DEFAULT (0))`);
         await queryRunner.query(`CREATE TABLE "meal_product" ("mealId" integer NOT NULL, "productId" integer NOT NULL, "quantity" decimal(4,1) NOT NULL, PRIMARY KEY ("mealId", "productId"))`);
         await queryRunner.query(`CREATE TABLE "product_portions" ("productId" integer NOT NULL, "type" varchar NOT NULL, "value" integer NOT NULL, CONSTRAINT "UQ_422bab5f9d32229ab5eec6f1631" UNIQUE ("productId", "type"), PRIMARY KEY ("productId", "type"))`);
-        await queryRunner.query(`CREATE TABLE "product_images" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "url" varchar NOT NULL, "productId" integer)`);
+        await queryRunner.query(`CREATE TABLE "product_images" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "url" varchar NOT NULL, "productId" integer NOT NULL, CONSTRAINT "UQ_9fc901c53aea752d77b5392c012" UNIQUE ("url", "productId"))`);
         await queryRunner.query(`CREATE TABLE "product" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar NOT NULL, "barcode" text, "unit" text NOT NULL DEFAULT ('g'), "createdAt" text NOT NULL DEFAULT (CURRENT_TIMESTAMP), "updatedAt" text NOT NULL DEFAULT (CURRENT_TIMESTAMP), "isVerified" boolean, "userId" integer, "macroCarbs" decimal(6,2) NOT NULL DEFAULT (0), "macroProts" decimal(6,2) NOT NULL DEFAULT (0), "macroFats" decimal(6,2) NOT NULL DEFAULT (0), "macroKcal" decimal(6,2) NOT NULL DEFAULT (0), CONSTRAINT "UQ_3e46cda47a776b779fa6faaed10" UNIQUE ("barcode", "isVerified"), CONSTRAINT "UQ_c1dd1975fe160d2190161668c52" UNIQUE ("barcode", "userId"), CONSTRAINT "UQ_905c0fed2f9ceb88491e43331ec" UNIQUE ("name", "isVerified"), CONSTRAINT "CHK_fc8bb6dce3ee11546ec5f9d2bb" CHECK ("unit" IN ('g','ml')))`);
         await queryRunner.query(`CREATE TABLE "temporary_profile" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "weight" tinyint NOT NULL, "height" tinyint NOT NULL, "age" tinyint NOT NULL, "male" boolean NOT NULL, "weightGoal" text NOT NULL, "userId" integer NOT NULL, "macroNeedsCarbs" decimal(6,2) NOT NULL DEFAULT (0), "macroNeedsProts" decimal(6,2) NOT NULL DEFAULT (0), "macroNeedsFats" decimal(6,2) NOT NULL DEFAULT (0), "macroNeedsKcal" decimal(6,2) NOT NULL DEFAULT (0), CONSTRAINT "REL_a24972ebd73b106250713dcddd" UNIQUE ("userId"), CONSTRAINT "CHK_e13c7952dc8655e48c8f83608a" CHECK ("weightGoal" IN ('decrease','maintain','increase')), CONSTRAINT "FK_a24972ebd73b106250713dcddd9" FOREIGN KEY ("userId") REFERENCES "user" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION)`);
         await queryRunner.query(`INSERT INTO "temporary_profile"("id", "weight", "height", "age", "male", "weightGoal", "userId", "macroNeedsCarbs", "macroNeedsProts", "macroNeedsFats", "macroNeedsKcal") SELECT "id", "weight", "height", "age", "male", "weightGoal", "userId", "macroNeedsCarbs", "macroNeedsProts", "macroNeedsFats", "macroNeedsKcal" FROM "profile"`);
@@ -26,7 +26,7 @@ export class Initial1568116851233 implements MigrationInterface {
         await queryRunner.query(`INSERT INTO "temporary_product_portions"("productId", "type", "value") SELECT "productId", "type", "value" FROM "product_portions"`);
         await queryRunner.query(`DROP TABLE "product_portions"`);
         await queryRunner.query(`ALTER TABLE "temporary_product_portions" RENAME TO "product_portions"`);
-        await queryRunner.query(`CREATE TABLE "temporary_product_images" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "url" varchar NOT NULL, "productId" integer, CONSTRAINT "FK_b367708bf720c8dd62fc6833161" FOREIGN KEY ("productId") REFERENCES "product" ("id") ON DELETE CASCADE ON UPDATE NO ACTION)`);
+        await queryRunner.query(`CREATE TABLE "temporary_product_images" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "url" varchar NOT NULL, "productId" integer NOT NULL, CONSTRAINT "UQ_9fc901c53aea752d77b5392c012" UNIQUE ("url", "productId"), CONSTRAINT "FK_b367708bf720c8dd62fc6833161" FOREIGN KEY ("productId") REFERENCES "product" ("id") ON DELETE CASCADE ON UPDATE NO ACTION)`);
         await queryRunner.query(`INSERT INTO "temporary_product_images"("id", "url", "productId") SELECT "id", "url", "productId" FROM "product_images"`);
         await queryRunner.query(`DROP TABLE "product_images"`);
         await queryRunner.query(`ALTER TABLE "temporary_product_images" RENAME TO "product_images"`);
@@ -37,6 +37,30 @@ export class Initial1568116851233 implements MigrationInterface {
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query(`ALTER TABLE "product" RENAME TO "temporary_product"`);
+        await queryRunner.query(`CREATE TABLE "product" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar NOT NULL, "barcode" text, "unit" text NOT NULL DEFAULT ('g'), "createdAt" text NOT NULL DEFAULT (CURRENT_TIMESTAMP), "updatedAt" text NOT NULL DEFAULT (CURRENT_TIMESTAMP), "isVerified" boolean, "userId" integer, "macroCarbs" decimal(6,2) NOT NULL DEFAULT (0), "macroProts" decimal(6,2) NOT NULL DEFAULT (0), "macroFats" decimal(6,2) NOT NULL DEFAULT (0), "macroKcal" decimal(6,2) NOT NULL DEFAULT (0), CONSTRAINT "UQ_3e46cda47a776b779fa6faaed10" UNIQUE ("barcode", "isVerified"), CONSTRAINT "UQ_c1dd1975fe160d2190161668c52" UNIQUE ("barcode", "userId"), CONSTRAINT "UQ_905c0fed2f9ceb88491e43331ec" UNIQUE ("name", "isVerified"), CONSTRAINT "CHK_fc8bb6dce3ee11546ec5f9d2bb" CHECK ("unit" IN ('g','ml')))`);
+        await queryRunner.query(`INSERT INTO "product"("id", "name", "barcode", "unit", "createdAt", "updatedAt", "isVerified", "userId", "macroCarbs", "macroProts", "macroFats", "macroKcal") SELECT "id", "name", "barcode", "unit", "createdAt", "updatedAt", "isVerified", "userId", "macroCarbs", "macroProts", "macroFats", "macroKcal" FROM "temporary_product"`);
+        await queryRunner.query(`DROP TABLE "temporary_product"`);
+        await queryRunner.query(`ALTER TABLE "product_images" RENAME TO "temporary_product_images"`);
+        await queryRunner.query(`CREATE TABLE "product_images" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "url" varchar NOT NULL, "productId" integer NOT NULL, CONSTRAINT "UQ_9fc901c53aea752d77b5392c012" UNIQUE ("url", "productId"))`);
+        await queryRunner.query(`INSERT INTO "product_images"("id", "url", "productId") SELECT "id", "url", "productId" FROM "temporary_product_images"`);
+        await queryRunner.query(`DROP TABLE "temporary_product_images"`);
+        await queryRunner.query(`ALTER TABLE "product_portions" RENAME TO "temporary_product_portions"`);
+        await queryRunner.query(`CREATE TABLE "product_portions" ("productId" integer NOT NULL, "type" varchar NOT NULL, "value" integer NOT NULL, CONSTRAINT "UQ_422bab5f9d32229ab5eec6f1631" UNIQUE ("productId", "type"), PRIMARY KEY ("productId", "type"))`);
+        await queryRunner.query(`INSERT INTO "product_portions"("productId", "type", "value") SELECT "productId", "type", "value" FROM "temporary_product_portions"`);
+        await queryRunner.query(`DROP TABLE "temporary_product_portions"`);
+        await queryRunner.query(`ALTER TABLE "meal_product" RENAME TO "temporary_meal_product"`);
+        await queryRunner.query(`CREATE TABLE "meal_product" ("mealId" integer NOT NULL, "productId" integer NOT NULL, "quantity" decimal(4,1) NOT NULL, PRIMARY KEY ("mealId", "productId"))`);
+        await queryRunner.query(`INSERT INTO "meal_product"("mealId", "productId", "quantity") SELECT "mealId", "productId", "quantity" FROM "temporary_meal_product"`);
+        await queryRunner.query(`DROP TABLE "temporary_meal_product"`);
+        await queryRunner.query(`ALTER TABLE "meal" RENAME TO "temporary_meal"`);
+        await queryRunner.query(`CREATE TABLE "meal" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar NOT NULL, "date" text NOT NULL DEFAULT (CURRENT_TIMESTAMP), "userId" integer, "macroCarbs" decimal(6,2) NOT NULL DEFAULT (0), "macroProts" decimal(6,2) NOT NULL DEFAULT (0), "macroFats" decimal(6,2) NOT NULL DEFAULT (0), "macroKcal" decimal(6,2) NOT NULL DEFAULT (0))`);
+        await queryRunner.query(`INSERT INTO "meal"("id", "name", "date", "userId", "macroCarbs", "macroProts", "macroFats", "macroKcal") SELECT "id", "name", "date", "userId", "macroCarbs", "macroProts", "macroFats", "macroKcal" FROM "temporary_meal"`);
+        await queryRunner.query(`DROP TABLE "temporary_meal"`);
+        await queryRunner.query(`ALTER TABLE "profile" RENAME TO "temporary_profile"`);
+        await queryRunner.query(`CREATE TABLE "profile" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "weight" tinyint NOT NULL, "height" tinyint NOT NULL, "age" tinyint NOT NULL, "male" boolean NOT NULL, "weightGoal" text NOT NULL, "userId" integer NOT NULL, "macroNeedsCarbs" decimal(6,2) NOT NULL DEFAULT (0), "macroNeedsProts" decimal(6,2) NOT NULL DEFAULT (0), "macroNeedsFats" decimal(6,2) NOT NULL DEFAULT (0), "macroNeedsKcal" decimal(6,2) NOT NULL DEFAULT (0), CONSTRAINT "REL_a24972ebd73b106250713dcddd" UNIQUE ("userId"), CONSTRAINT "CHK_e13c7952dc8655e48c8f83608a" CHECK ("weightGoal" IN ('decrease','maintain','increase')))`);
+        await queryRunner.query(`INSERT INTO "profile"("id", "weight", "height", "age", "male", "weightGoal", "userId", "macroNeedsCarbs", "macroNeedsProts", "macroNeedsFats", "macroNeedsKcal") SELECT "id", "weight", "height", "age", "male", "weightGoal", "userId", "macroNeedsCarbs", "macroNeedsProts", "macroNeedsFats", "macroNeedsKcal" FROM "temporary_profile"`);
+        await queryRunner.query(`DROP TABLE "temporary_profile"`);
         await queryRunner.query(`DROP TABLE "product"`);
         await queryRunner.query(`DROP TABLE "product_images"`);
         await queryRunner.query(`DROP TABLE "product_portions"`);
