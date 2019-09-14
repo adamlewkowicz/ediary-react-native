afc06f8161767a2aa22dc86afc532449aa6cb7d6
alk831 <adam54111@gmail.com>
2019-09-14 13:10:32 +0200

    update Meal spec naming

--- src/database/entities/Meal/index.spec.tsx
+++ src/database/entities/Meal/index.spec.tsx
@@ -4,31 +4,31 @@ import dayjs from 'dayjs';
 import { DATE_FORMAT } from '../../../common/consts';
 
 test('Meal.getMacroSummary - groups meals by day and returns summed macro values', async () => {
-  const dayMock = '2019-01-01';
-  const nextDayMock = '2019-01-02';
+  const dayAMock = '2019-01-01';
+  const dayBMock = '2019-01-03';
 
-  const date = new Date(dayMock);
-  const nextDayDate = new Date(nextDayMock);
+  const dateA = new Date(dayAMock);
+  const dateB = new Date(dayBMock);
 
-  const dateString = dayjs(date).format(DATE_FORMAT);
-  const nextDayDateString = dayjs(nextDayDate).format(DATE_FORMAT);
+  const dateAString = dayjs(dateA).format(DATE_FORMAT);
+  const dateBString = dayjs(dateB).format(DATE_FORMAT);
 
-  await Meal.save({ name: 'A', date: dateString, macro: { carbs: 40, kcal: 10 }});
-  await Meal.save({ name: 'B', date: dateString, macro: { carbs: 40, kcal: 10 }});
-  await Meal.save({ name: 'C', date: nextDayDateString, macro: { carbs: 40, kcal: 10 }});
+  await Meal.save({ name: 'A1', date: dateAString, macro: { carbs: 40, kcal: 10 }});
+  await Meal.save({ name: 'A2', date: dateAString, macro: { carbs: 40, kcal: 10 }});
+  await Meal.save({ name: 'B1', date: dateBString, macro: { carbs: 40, kcal: 10 }});
 
-  const result = await Meal.getMacroSummary(getDayFromDate(date));
+  const result = await Meal.getMacroSummary(getDayFromDate(dateA));
 
   expect(result).toEqual([
     {
-      day: dayMock,
+      day: dayAMock,
       carbs: 80,
       prots: 0,
       fats: 0,
       kcal: 20
     },
     {
-      day: nextDayMock,
+      day: dayBMock,
       carbs: 40,
       prots: 0,
       fats: 0,
