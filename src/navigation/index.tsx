ac58ab8d05c6826b1ffb26d8801c37ee64bb5fba
alk831 <adam54111@gmail.com>
2019-09-16 19:33:38 +0200

    update bottom tab styling & add icons

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
   createStackNavigator,
   createAppContainer,
@@ -15,6 +16,9 @@ import {
 } from '../screens';
 import { theme } from '../common/theme';
 import { Screen } from '../types';
+import { DishIcon, ReportIcon } from '../components/Icons';
+
+const BAR_ICON_SIZE = 22;
 
 export function createHomeStack(
   initialRouteName: Screen = 'Home'
@@ -38,10 +42,43 @@ export function createHomeStack(
 const HomeStack = createHomeStack();
 
 const MainStack = createBottomTabNavigator({
-  Home: HomeStack,
-  DiarySummary,
+  Home: {
+    screen: HomeStack,
+    navigationOptions: {
+      header: null,
+      tabBarLabel: 'Dziennik',
+      tabBarIcon: ({ tintColor }: any) => (
+        <DishIcon
+          width={BAR_ICON_SIZE}
+          height={BAR_ICON_SIZE}
+          fill={tintColor}
+        />
+      ),
+    }
+  },
+  DiarySummary: {
+    screen: DiarySummary,
+    navigationOptions: {
+      tabBarLabel: 'Podsumowanie',
+      tabBarIcon: ({ tintColor }: any) => (
+        <ReportIcon
+          width={BAR_ICON_SIZE}
+          height={BAR_ICON_SIZE}
+          fill={tintColor}
+        />
+      ),
+    }
+  },
 }, {
   initialRouteName: 'Home',
+  tabBarOptions: {
+    showIcon: true,
+    activeTintColor: theme.color.blue20,
+    labelStyle: {
+      fontSize: theme.fontSize.tiny,
+      fontFamily: theme.fontWeight.regular,
+    }
+  }
 });
 
 const RootNavigator = createSwitchNavigator({
