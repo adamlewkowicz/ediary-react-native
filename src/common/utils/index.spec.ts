c4889561da1a34b010f22d38d39ba4a43a5495e9
alk831 <adam54111@gmail.com>
2019-09-29 20:18:48 +0200

    add utils spec

--- src/common/utils/index.spec.ts
+++ src/common/utils/index.spec.ts
@@ -1,4 +1,43 @@
-import { calcMacroNeedsLeft } from '.'
+import {
+  calcMacroNeedsLeft,
+  debounce_,
+  mapAsyncSequence,
+  filterByUniqueId,
+  findOrFail,
+} from '.';
+
+test('debounce - function gets called only once', () => {
+  jest.useFakeTimers();
+  const actionMock = jest.fn();
+  const debouncer = debounce_();
+
+  debouncer(actionMock, 150);
+  debouncer(actionMock, 150);
+
+  jest.runAllTimers();
+
+  expect(actionMock).toHaveBeenCalledTimes(1);
+});
+
+test('mapAsyncSequence - runs promises in sequence [todo]', () => {
+  // todo
+});
+
+test('filterByUniqueId - filters items by unique id', () => {
+  const items = [{ id: 1, }, { id: 1 }];
+  const filteredItems = items.filter(filterByUniqueId);
+  
+  expect(filteredItems.length).toBeLessThan(items.length);
+});
+
+test('findOrFail - throws error if no item was found', () => {
+  const items = [{ name: 'A' }];
+  try {
+    findOrFail(items, item => item.name === 'B');
+  } catch (error) {
+    expect(error).toBeInstanceOf(Error);
+  }
+});
 
 test('calcMacroNeedsLeft', () => {
   const macroEaten = {
@@ -19,5 +58,5 @@ test('calcMacroNeedsLeft', () => {
     macroNeeded
   );
 
-  expect(result.carbs.diff).toEqual(macroNeeded.carbs - macroEaten.carbs);
+  expect(result.carbs.diff).toBeGreaterThan(0);
 });
