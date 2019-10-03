221acbfc05bc0581d95ae8783d3d56d0488178eb
alk831 <adam54111@gmail.com>
2019-10-03 15:30:32 +0200

    refactor: getProductsFromAction util

--- src/store/reducers/productHistory/helpers.ts
+++ src/store/reducers/productHistory/helpers.ts
@@ -8,14 +8,17 @@ import { ProductHistoryAction } from '../../actions/types/productHistory';
 
 export const getProductsFromAction = (action: ProductHistoryAction): Product[] => {
   switch(action.type) {
-    case PRODUCT_HISTORY_RECENT_ADDED: return action.payload;
-    case MEAL_PRODUCT_ADDED: return action.meta.rawProduct
-      ? [action.meta.rawProduct]
-      : [];
+    case PRODUCT_HISTORY_RECENT_ADDED:
+      return action.payload;
+    case MEAL_PRODUCT_ADDED:
+      if (action.meta.rawProduct) {
+        return [action.meta.rawProduct];
+      }
     case MEAL_ADDED: 
       const { mealProducts = [] } = action.payload;
       const products = mealProducts.flatMap(mealProduct => mealProduct.product);
       return products;
-    default: return [];
+    default:
+      return [];
   }
 }
