8e4180d45a92059855c433a9db07794ba83d76dd
alk831 <adam54111@gmail.com>
2019-10-03 14:54:59 +0200

    add sortByMostAccurateName spec

--- src/common/utils/index.spec.ts
+++ src/common/utils/index.spec.ts
@@ -3,10 +3,11 @@ import {
   debounce_,
   mapAsyncSequence,
   filterByUniqueId,
-  findOrFail
-} from ".";
+  findOrFail,
+  sortByMostAccurateName,
+} from '.';
 
-test("debounce - debounced function gets called only once", () => {
+test('debounce - debounced function gets called only once', () => {
   jest.useFakeTimers();
   const actionMock = jest.fn();
   const debounceHandler = debounce_();
@@ -22,7 +23,7 @@ test("debounce - debounced function gets called only once", () => {
   jest.clearAllTimers();
 });
 
-test("mapAsyncSequence - runs promises in a sequence", async () => {
+test('mapAsyncSequence - runs promises in a sequence', async () => {
   const promiseMock = jest.fn().mockImplementation(async () => {});
   const dataMock = [1, 2];
 
@@ -33,23 +34,23 @@ test("mapAsyncSequence - runs promises in a sequence", async () => {
   expect(promiseMock).toHaveBeenNthCalledWith(2, dataMock[1]);
 });
 
-test("filterByUniqueId - filters items by unique id", () => {
+test('filterByUniqueId - filters items by unique id', () => {
   const items = [{ id: 1 }, { id: 1 }];
   const filteredItems = items.filter(filterByUniqueId);
 
   expect(filteredItems.length).toBeLessThan(items.length);
 });
 
-test("findOrFail - throws error if no item was found", () => {
-  const items = [{ name: "A" }];
+test('findOrFail - throws error if no item was found', () => {
+  const items = [{ name: 'A' }];
   try {
-    findOrFail(items, item => item.name === "B");
+    findOrFail(items, item => item.name === 'B');
   } catch (error) {
     expect(error).toBeInstanceOf(Error);
   }
 });
 
-test("calcMacroNeedsLeft", () => {
+test('calcMacroNeedsLeft', () => {
   const macroEaten = {
     carbs: 400,
     prots: 0,
@@ -67,12 +68,18 @@ test("calcMacroNeedsLeft", () => {
 
   expect(result.carbs.diff).toBeGreaterThan(0);
   expect(result.carbs.ratio).toBeGreaterThan(0);
-  expect(result.carbs).toMatchInlineSnapshot(`
-    Object {
-      "diff": 600,
-      "eaten": 400,
-      "needed": 1000,
-      "ratio": 40,
-    }
-  `);
+  expect(result.carbs).toMatchSnapshot();
+});
+
+test('sortByMostAccurateName - sorts items by most matching name', () => {
+  const name = 'orange';
+  const dataMock = [
+    { name: 'Orange a' },
+    { name: 'Eng' },
+    { name: 'Orange' },
+  ];
+
+  const result = dataMock.sort(sortByMostAccurateName(name));
+
+  expect(result).toMatchSnapshot();
 });
