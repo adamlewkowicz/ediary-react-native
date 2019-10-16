92d4ae6dfdac1993bc587e83eec6d8aae136afe9
alk831 <adam54111@gmail.com>
2019-10-16 10:39:38 +0200

    refactor utils specs

--- src/common/utils/index.spec.ts
+++ src/common/utils/index.spec.ts
@@ -7,77 +7,80 @@ import {
   sortByMostAccurateName,
 } from '.';
 
-test('debounce - debounced function gets called only once', () => {
-  jest.useFakeTimers();
-  const actionMock = jest.fn();
-  const debounceHandler = debounce();
-  const debounceTime = 150;
-
-  debounceHandler(actionMock, debounceTime);
-  debounceHandler(actionMock, debounceTime);
-
-  jest.advanceTimersByTime(debounceTime);
-
-  expect(actionMock).toHaveBeenCalledTimes(1);
+describe('debounce()', () => {
+  it('should call debounced function only once', () => {
+    jest.useFakeTimers();
+    const actionMock = jest.fn();
+    const debounceHandler = debounce();
+    const debounceTime = 150;
+  
+    debounceHandler(actionMock, debounceTime);
+    debounceHandler(actionMock, debounceTime);
+    jest.advanceTimersByTime(debounceTime);
+  
+    expect(actionMock).toHaveBeenCalledTimes(1);
+  });
 });
 
-test('mapAsyncSequence - runs promises in a sequence', async () => {
-  const promiseMock = jest.fn().mockImplementation(async () => {});
-  const dataMock = [1, 2];
-
-  await mapAsyncSequence(dataMock, value => promiseMock(value));
-
-  expect(promiseMock).toHaveBeenCalledTimes(dataMock.length);
-  expect(promiseMock).toHaveBeenNthCalledWith(1, dataMock[0]);
-  expect(promiseMock).toHaveBeenNthCalledWith(2, dataMock[1]);
+describe('mapAsyncSequence()', () => {
+  it('should run promises in a sequence', async () => {
+    const promiseMock = jest.fn().mockImplementation(async () => {});
+    const dataMock = [1, 2];
+  
+    await mapAsyncSequence(dataMock, value => promiseMock(value));
+  
+    expect(promiseMock).toHaveBeenCalledTimes(dataMock.length);
+    expect(promiseMock).toHaveBeenNthCalledWith(1, dataMock[0]);
+    expect(promiseMock).toHaveBeenNthCalledWith(2, dataMock[1]);
+  });
 });
 
-test('filterByUniqueId - filters items by unique id', () => {
-  const items = [{ id: 1 }, { id: 1 }];
-  const filteredItems = items.filter(filterByUniqueId);
+describe('filterByUniqueId()', () => {
+  it('should filter items by unique id', () => {
+    const items = [{ id: 1 }, { id: 1 }];
 
-  expect(filteredItems.length).toBeLessThan(items.length);
+    const filteredItems = items.filter(filterByUniqueId);
+  
+    expect(filteredItems.length).toBeLessThan(items.length);
+  });
 });
 
-test('findOrFail - throws error if no item was found', () => {
-  const items = [{ name: 'A' }];
-  try {
-    findOrFail(items, item => item.name === 'B');
-  } catch (error) {
-    expect(error).toBeInstanceOf(Error);
-  }
+describe('findOrFail()', () => {
+  it('should throw error if no item was found', () => {
+    const items = [{ name: 'A' }];
+    try {
+      findOrFail(items, item => item.name === 'B');
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+    }
+  });
 });
 
-test('calcMacroNeedsLeft', () => {
-  const macroEaten = {
-    carbs: 400,
-    prots: 0,
-    fats: 0,
-    kcal: 0
-  };
-  const macroNeeded = {
-    carbs: 1000,
-    prots: 0,
-    fats: 0,
-    kcal: 0
-  };
-
-  const result = calcMacroNeedsLeft(macroEaten, macroNeeded);
-
-  expect(result.carbs.diff).toBeGreaterThan(0);
-  expect(result.carbs.ratio).toBeGreaterThan(0);
-  expect(result.carbs).toMatchSnapshot();
+describe('calcMacroNeedsLeft()', () => {
+  it('should calculate macro differences properly', () => {
+    const baseMacro = { carbs: 0, prots: 0, fats: 0, kcal: 0 };
+    const macroEaten = { ...baseMacro, carbs: 400 };
+    const macroNeeded = { ...baseMacro, carbs: 1000 };
+  
+    const result = calcMacroNeedsLeft(macroEaten, macroNeeded);
+  
+    expect(result.carbs.diff).toBeGreaterThan(0);
+    expect(result.carbs.ratio).toBeGreaterThan(0);
+    expect(result.carbs).toMatchSnapshot();
+  });
 });
 
-test('sortByMostAccurateName - sorts items by most matching name', () => {
-  const name = 'orange';
-  const dataMock = [
-    { name: 'Orange a' },
-    { name: 'Eng' },
-    { name: 'Orange' },
-  ];
-
-  const result = dataMock.sort(sortByMostAccurateName(name));
-
-  expect(result).toMatchSnapshot();
+describe('sortByMostAccurateName()', () => {
+  it('should sort items by most matching name', () => {
+    const name = 'orange';
+    const dataMock = [
+      { name: 'Orange a' },
+      { name: 'Eng' },
+      { name: 'Orange' },
+    ];
+  
+    const result = dataMock.sort(sortByMostAccurateName(name));
+  
+    expect(result).toMatchSnapshot();
+  });
 });
