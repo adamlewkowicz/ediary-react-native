ac88ec8a3d5379c1a113879a6d962ecc722318b4
alk831 <adam54111@gmail.com>
2019-10-22 16:36:08 +0200

    use navigation params hook

--- src/hooks/useNavigationParams.ts
+++ src/hooks/useNavigationParams.ts
@@ -5,19 +5,21 @@ export const useNavigationParams = <
   T extends object,
   K extends keyof T = keyof T
 >(
-  paramNames: K[]
+  paramNames: readonly K[]
 ): T => {
   type ParamEntries = [K, T[K]][];
-
+  
   const navigation = useNavigation();
 
-  const [params] = useState<T>(() => {
+  const getParamsObject = (): T => {
     const paramEntries = paramNames.map<ParamEntries>(paramName => [
       paramName, navigation.getParam(paramName as string)
     ]);
     const paramsObject: T = Object.fromEntries(paramEntries);
     return paramsObject;
-  });
+  }
+
+  const [params] = useState<T>(getParamsObject);
 
   return params;
 }
