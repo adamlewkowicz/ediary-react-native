cd41d488a8715c1f67c328fdfc85e2cdb0f62b37
alk831 <adam54111@gmail.com>
2019-10-24 12:52:26 +0200

    refactor Product specs

--- src/database/entities/Product/index.spec.ts
+++ src/database/entities/Product/index.spec.ts
@@ -6,47 +6,44 @@ describe('Product', () => {
 
   describe('findMostUsed()' ,() => {
     it('should find most used products', async () => {
-      const product = await Product.save({ name: 'Cucumber' });
-      await Meal.createWithProductId({ name: 'Cucumber soup' }, product.id);
+      const productMock = await Product.save({ name: 'Cucumber' });
+      await Meal.createWithProductId({ name: 'Cucumber soup' }, productMock.id);
 
       const [foundProduct] = await Product.findMostUsed();
     
-      expect(foundProduct.productId).toBe(product.id);
-      expect(foundProduct.count).toBe(1);
+      expect(foundProduct.productId).toEqual(productMock.id);
+      expect(foundProduct.count).toEqual(1);
     });
   });
 
   describe('findRecentlyUsed()', () => {
     it('should find recently used products', async () => {
-      const product = await Product.save({ name: 'Cucumber' });
-      await Meal.createWithProductId({ name: 'Cucumber soup' }, product.id);
+      const productMock = await Product.save({ name: 'Cucumber' });
+      await Meal.createWithProductId({ name: 'Cucumber soup' }, productMock.id);
 
       const [foundProduct] = await Product.findRecentlyUsed();
     
-      expect(foundProduct.id).toBe(product.id);
+      expect(foundProduct.id).toEqual(productMock.id);
     });
   });
 
   describe('saveNormalizedProduct()', () => {
-    it('should properly parse and save normalized product', async () => {
+    it('should properly save normalized product', async () => {
       const normalizedProduct: NormalizedProduct = {
         _id: 1,
         name: 'Abc',
-        carbs: 0,
-        prots: 0,
-        fats: 0,
-        kcal: 0,
+        macro: {
+          carbs: 10,
+          prots: 10,
+          fats: 10,
+          kcal: 10,
+        },
+        portion: 100,
         portions: [],
       }
-      const saveSpy = jest.spyOn(Product, 'save');
-      const result = await Product.saveNormalizedProduct(normalizedProduct);
+      const savedProduct = await Product.saveNormalizedProduct(normalizedProduct);
 
-      expect(saveSpy).toHaveBeenCalledTimes(1);
-      expect(result).toMatchSnapshot({
-        id: expect.any(Number),
-        updatedAt: expect.any(String),
-        createdAt: expect.any(String),
-      });
+      expect(savedProduct).toBeInstanceOf(Product);
     });
   });
   
