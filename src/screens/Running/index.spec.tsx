519079ad01451bf8cb93ab4edaf24c15dafba5fc
alk831 <adam54111@gmail.com>
2019-10-28 14:20:40 +0100

    refactor specs

--- src/screens/Running/index.spec.tsx
+++ src/screens/Running/index.spec.tsx
@@ -1,28 +1,50 @@
+import React from 'react';
 import { render, fireEvent } from '@testing-library/react-native';
 import { RunningScreen } from './';
+import Geolocation, { GeolocationResponse } from '@react-native-community/geolocation';
 
 describe('<RunningScreen />', () => {
 
   it('should increment training duration', () => {
     jest.useFakeTimers();
-    const utils = render(<RunningScreen />);
+    const ctx = render(<RunningScreen />);
 
-    const trainingDurationText = utils.getByLabelText('Czas trwania treningu');
+    const trainingDurationText = ctx.getByLabelText('Czas trwania treningu');
     jest.advanceTimersByTime(1000);
 
-    expect(trainingDurationText).not.toEqual('0.00');
+    expect(trainingDurationText).toEqual('00:00:01');
   });
 
-  it('should pause training when clicked on pause button', () => {
-    const utils = render(<RunningScreen />);
+  it('should pause training when clicked on pause a button', () => {
+    jest.useFakeTimers();
+    const ctx = render(<RunningScreen />);
 
-    const pauseTrainingButton = utils.getByLabelText('')
+    const pauseTrainingButton = ctx.getByLabelText('Zatrzymaj/kontynuuj trening');
 
-    fireEvent.press(pauseTrainingButton, {  })
+    fireEvent.pressIn(pauseTrainingButton);
+    jest.advanceTimersByTime(500);
+
   });
 
-  it('should show running session distance', () => {
+  it('should calculate correct session distance', () => {
+    const geoLocationMock = jest.spyOn(Geolocation, 'watchPosition')
+      .mockImplementationOnce(success => {
+        success({
+          timestamp: 1,
+          coords: {
+            longitude: 10000,
+            latitude: 10000,
+          }
+        } as GeolocationResponse)
+        const watchId = 1;
+        return watchId;
+      })
+    const ctx = render(<RunningScreen />);
+    
+    const trainingDistanceText = ctx.findByLabelText('Przebyty dystans');
 
+    expect(geoLocationMock).toHaveBeenCalledTimes(1);
+    expect(trainingDistanceText).not.toEqual('0.00');
   });
 
 });
