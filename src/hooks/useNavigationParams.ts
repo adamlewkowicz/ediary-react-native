6357f28ce05326f54e9518d53822382270f2be18
alk831 <adam54111@gmail.com>
2020-02-15 18:16:26 +0100

    empty obj as default

--- src/hooks/useNavigationParams.ts
+++ src/hooks/useNavigationParams.ts
@@ -1,6 +1,6 @@
 import { useNavigation } from 'react-navigation-hooks';
 
 export const useNavigationParams = <T extends object>(): T => {
-  const navigation = useNavigation();
-  return navigation.state.params as T;
+  const { state: { params = {} }} = useNavigation();
+  return params as T;
 }
