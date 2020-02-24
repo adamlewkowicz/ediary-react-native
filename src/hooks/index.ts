09d3f1a6895e51fbe18cbfec7fda96f9d19173d3
alk831 <adam54111@gmail.com>
2020-02-24 17:53:11 +0100

    update hook imports [skip ci]

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,5 @@ export * from './useUserId';
 export * from './useConnected';
 export * from './useAfterInteractions';
 export * from './useIdleStatus';
+export * from './useDebouncedValue';
+export * from './useProductsSearch';
