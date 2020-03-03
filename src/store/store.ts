d1f506c3551f538afcfaf70e0deb22d265c0f30d
alk831 <adam54111@gmail.com>
2020-03-03 21:17:05 +0100

    rename store module

---
+++ src/store/store.ts
@@ -0,0 +1,44 @@
+import { createStore, applyMiddleware, Store, Action } from 'redux';
+import { rootReducer } from './reducers';
+import thunk, { ThunkAction } from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import * as Actions from './actions';
+import * as Selectors from './selectors';
+
+export function configureStore(
+  initialState?: Partial<StoreState>
+): Store<StoreState> {
+
+  const store = createStore(
+    rootReducer,
+    initialState,
+    composeWithDevTools(
+      applyMiddleware(
+        thunk
+      )
+    )
+  );
+
+  if (module.hot) {
+    module.hot.accept(() => {
+      // eslint-disable-next-line
+      const { rootReducer } = require('./reducers');
+      store.replaceReducer(rootReducer);
+    });
+  }
+
+  return store;
+}
+
+export const store = configureStore();
+
+export type StoreState = ReturnType<typeof rootReducer>;
+export { StoreState as AppState };
+
+export type Thunk<
+  R = void,
+  A extends Action = Action<string>
+> = ThunkAction<R, StoreState, void, A>;
+
+export { Actions };
+export { Selectors };
