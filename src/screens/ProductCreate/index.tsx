aa7287cbaab2cec02346f444a6956292e933fd46
alk831 <adam54111@gmail.com>
2020-03-17 21:21:06 +0100

    add prots input ref

--- src/screens/ProductCreate/index.tsx
+++ src/screens/ProductCreate/index.tsx
@@ -148,6 +148,7 @@ export const ProductCreateScreen = (props: ProductCreateScreenProps) => {
             label="Białko"
             placeholder="0"
             metaText="g"
+            ref={protsInputRef}
             onSubmitEditing={fatsInputRef.current?.focus}
           />
           <Group.Container>
