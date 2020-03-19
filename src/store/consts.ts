bd04a1c12d004da0d890ee6235cb98addab769c6
alk831 <adam54111@gmail.com>
2020-03-19 22:59:42 +0100

    rename action name

--- src/store/consts.ts
+++ src/store/consts.ts
@@ -5,7 +5,7 @@ export const MEAL_OPEN_TOGGLED = 'MEAL_OPEN_TOGGLED' as const;
 export const MEALS_LOADED = 'MEALS_LOADED' as const;
 export const MEAL_PRODUCT_ADDED = 'MEAL_PRODUCT_ADDED' as const;
 export const MEAL_PRODUCT_DELETED = 'MEAL_PRODUCT_DELETED' as const;
-export const PRODUCT_QUANTITY_UPDATED = 'PRODUCT_UPDATED' as const;
+export const PRODUCT_QUANTITY_UPDATED = 'PRODUCT_QUANTITY_UPDATED' as const;
 
 // Application
 export const APP_DATE_UPDATED = 'APP_DATE_UPDATED' as const;
