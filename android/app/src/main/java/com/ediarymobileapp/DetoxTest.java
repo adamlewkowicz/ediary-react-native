d8d169440ce2601299b05b44624da90807fbe1ea
alk831 <adam54111@gmail.com>
2020-03-26 17:48:27 +0100

    add detox config

---
+++ android/app/src/main/java/com/ediarymobileapp/DetoxTest.java
@@ -0,0 +1,27 @@
+// Replace this with your app's package
+package com.ediarymobileapp;
+
+import com.wix.detox.Detox;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class DetoxTest {
+
+    @Rule
+    // Replace 'MainActivity' with the value of android:name entry in 
+    // <activity> in AndroidManifest.xml
+    public ActivityTestRule<com.facebook.react.devsupport.DevSettingsActivity> mActivityRule = new ActivityTestRule<>(com.facebook.react.devsupport.DevSettingsActivity.class, false, false);
+
+    @Test
+    public void runDetoxTests() {
+        Detox.runTests(mActivityRule);
+    }
+}
