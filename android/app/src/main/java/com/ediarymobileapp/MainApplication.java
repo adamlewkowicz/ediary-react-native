577f73670d9047ebb45c98539bb88033b0d87af8
alk831 <adam54111@gmail.com>
2020-03-28 22:00:22 +0100

    native: remove rn svg linking

--- android/app/src/main/java/com/ediarymobileapp/MainApplication.java
+++ android/app/src/main/java/com/ediarymobileapp/MainApplication.java
@@ -7,7 +7,6 @@ import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.swmansion.reanimated.ReanimatedPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
