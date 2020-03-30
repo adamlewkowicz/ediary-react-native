8dc503b2ef0e3edffcac2be78a92ac5ecb8df8a1
alk831 <adam54111@gmail.com>
2020-03-30 22:11:47 +0200

    spacing

--- android/app/src/debug/java/com/ediarymobileapp/ReactNativeFlipper.java
+++ android/app/src/debug/java/com/ediarymobileapp/ReactNativeFlipper.java
@@ -22,6 +22,7 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.modules.network.NetworkingModule;
 import okhttp3.OkHttpClient;
+
 public class ReactNativeFlipper {
   public static void initializeFlipper(Context context, ReactInstanceManager reactInstanceManager) {
     if (FlipperUtils.shouldEnableFlipper(context)) {
