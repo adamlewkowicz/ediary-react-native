a905eb84705f36b087d949ec92ff95e85580c00f
alk831 <adam54111@gmail.com>
2020-04-12 19:27:53 +0200

    update spec

--- src/services/FriscoApi/index.spec.tsx
+++ src/services/FriscoApi/index.spec.tsx
@@ -1,22 +1,30 @@
-import { friscoApi } from '.';
+import { FriscoApi } from '.';
 import friscoResponseMock from './__mocks__/frisco-query.json';
 (global as any).fetch = require('node-fetch');
 
 jest.mock('node-fetch');
 
-test('findByQuery - product is normalized correctly', async () => {
-  const name = 'apple';
-  const fetchMock = fetch;
+describe('FriscoApi', () => {
 
-  (fetchMock as jest.Mock).mockImplementationOnce(async () => ({
-    ok: true,
-    async json() {
-      return friscoResponseMock;
-    }
-  }));
+  let friscoApi: FriscoApi;
 
-  const [normalizedProduct] = await friscoApi.findByBarcode(name);
+  beforeEach(() => friscoApi = new FriscoApi());
 
-  expect(fetchMock).toHaveBeenCalledTimes(1);
-  expect(normalizedProduct).toMatchSnapshot();
+  test('findByQuery - product is normalized correctly', async () => {
+    const name = 'apple';
+    const fetchMock = fetch;
+  
+    (fetchMock as jest.Mock).mockImplementationOnce(async () => ({
+      ok: true,
+      async json() {
+        return friscoResponseMock;
+      }
+    }));
+  
+    const [normalizedProduct] = await friscoApi.findByBarcode(name);
+  
+    expect(fetchMock).toHaveBeenCalledTimes(1);
+    expect(normalizedProduct).toMatchSnapshot();
+  });
+
 });
