68c9a23d517b4002dd233db12428b9559f9e3525
alk831 <adam54111@gmail.com>
2020-04-14 13:06:00 +0200

    remove compare spec

--- src/database/index.spec.ts
+++ src/database/index.spec.ts
@@ -25,32 +25,3 @@ test('entity generated sql matches snapshot', async () => {
     expect(prettySql).toMatchSnapshot(tbl_name);
   }
 });
-
-test('entity generated sql matches migration\'s sql', async () => {
-  const connection = getConnection();
-
-  await connection.dropDatabase();
-  await connection.synchronize();
-  const generatedSchema = await getDatabaseSchema();
-
-  await connection.dropDatabase();
-  await connection.runMigrations();
-  const migrationsSchema = await getDatabaseSchema();
-
-  generatedSchema.forEach((generatedTable, index) => {
-    const migratedTable = migrationsSchema[index];
-    const snapshotName = `[${generatedTable.tbl_name}] - generated to migrated`;
-    const diff = snapshotDiff(
-      migratedTable.prettySql,
-      generatedTable.prettySql,
-      {
-        contextLines: 0,
-        aAnnotation: 'Generated table',
-        bAnnotation: 'Migrated table',
-      }
-    );
-
-    expect(generatedTable.tbl_name).toEqual(migratedTable.tbl_name);
-    expect(diff).toMatchSnapshot(snapshotName);
-  });
-});
