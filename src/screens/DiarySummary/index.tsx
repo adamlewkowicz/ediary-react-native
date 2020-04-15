3672ae056064bb15643dc0be8ee5905ab972eaf4
alk831 <adam54111@gmail.com>
2020-04-15 19:57:09 +0200

    remove redundant screen props

--- src/screens/DiarySummary/index.tsx
+++ src/screens/DiarySummary/index.tsx
@@ -12,9 +12,7 @@ import { useFocusEffect } from '@react-navigation/native';
 import * as Utils from '../../utils';
 import { H1 } from '../../components';
 
-interface DiarySummaryScreenProps {}
-
-export const DiarySummaryScreen = (props: DiarySummaryScreenProps) => {
+export const DiarySummaryScreen = () => {
   const [macroSummary, setMacroSummary] = useState<MacroElements>(() => ({ ...MACRO }));
   const [historyRecords, setHistoryRecords] = useState<HistoryRecord[]>([]);
   const userMacroNeeds = useSelector(Selectors.getUserMacroNeeds);
