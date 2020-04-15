491e80b90e4e11f964d071216b9df2a004f20c35
alk831 <adam54111@gmail.com>
2020-04-15 20:52:49 +0200

    fix: search api context bind

--- src/services/ProductSearchApi/index.ts
+++ src/services/ProductSearchApi/index.ts
@@ -6,7 +6,7 @@ export class ProductSearchApi {
   private friscoApi = new FriscoApi();
   private ilewazyApi = new IlewazyApi();
 
-  findByBarcode = this.friscoApi.findByBarcode;
-  findByName = this.ilewazyApi.findByName;
+  findByBarcode = this.friscoApi.findByBarcode.bind(this.friscoApi);
+  findByName = this.ilewazyApi.findByName.bind(this.ilewazyApi);
 
 }
