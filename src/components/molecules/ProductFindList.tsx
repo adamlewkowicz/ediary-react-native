28e834ec7679350895a63d911d012ceae4e3f797
alk831 <adam54111@gmail.com>
2020-04-16 21:44:43 +0200

    update refresh controly style

--- src/components/molecules/ProductFindList.tsx
+++ src/components/molecules/ProductFindList.tsx
@@ -1,9 +1,10 @@
 
 import React from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, RefreshControl as NativeRefreshControl } from 'react-native';
 import { ProductOrNormalizedProduct, IProduct } from '../../database/entities';
 import { ItemSeparator } from '../atoms';
 import { ProductSearchItemMemo } from './ProductSearchItem';
+import styled from 'styled-components/native';
 
 export interface ProductFindListProps<T = ProductOrNormalizedProduct | IProduct> {
   data: T[]
@@ -15,19 +16,28 @@ export interface ProductFindListProps<T = ProductOrNormalizedProduct | IProduct>
 export const ProductFindList = (props: ProductFindListProps) => (
   <FlatList
     data={props.data}
+    keyExtractor={productKeyExtractor}
+    keyboardShouldPersistTaps="handled"
+    ItemSeparatorComponent={ItemSeparator}
     renderItem={({ item: product }) => (
       <ProductSearchItemMemo
         product={product}
         onSelect={props.onSelect}
       />
     )}
-    keyExtractor={productKeyExtractor}
-    keyboardShouldPersistTaps="handled"
-    ItemSeparatorComponent={ItemSeparator}
-    refreshing={props.isLoading ?? false}
-    onRefresh={() => props.onRefresh?.()}
+    refreshControl={(
+      <RefreshControl
+        refreshing={props.isLoading ?? false}
+        onRefresh={() => props.onRefresh?.()}
+      />
+    )}
   />
 );
+
+const RefreshControl = styled(NativeRefreshControl).attrs(props => ({
+  colors: [props.theme.color.highlight],
+  tintColor: props.theme.color.highlight
+}))``
 
 const productKeyExtractor = (product: ProductOrNormalizedProduct): string => {
   const productId = '_id' in product ? product._id : product.id;
