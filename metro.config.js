eae6ad75ab054c82b163fea4eeea66ba3798195b
alk831 <adam54111@gmail.com>
2020-04-23 20:32:06 +0200

    fix: wrong config setup

--- metro.config.js
+++ metro.config.js
@@ -13,24 +13,24 @@ module.exports = (async () => {
           inlineRequires: false
         }
       }),
-      babelTransformerPath: require.resolve("react-native-svg-transformer")
+      babelTransformerPath: require.resolve("react-native-svg-transformer"),
+      minifierPath: 'metro-minify-terser',
+      minifierConfig: {
+        // https://www.npmjs.com/package/terser#mangle-options
+        ecma: 8,
+        keep_classnames: true,
+        keep_fnames: true,
+        module: true,
+        mangle: {
+          module: true,
+          keep_classnames: true,
+          keep_fnames: true,
+        }
+      }
     },
     resolver: {
       assetExts: assetExts.filter(ext => ext !== "svg"),
       sourceExts: [...sourceExts, "svg"]
     },
-    minifierPath: 'metro-minify-terser',
-    minifierConfig: {
-      // https://www.npmjs.com/package/terser#mangle-options
-      ecma: 8,
-      keep_classnames: true,
-      keep_fnames: true,
-      module: true,
-      mangle: {
-        module: true,
-        keep_classnames: true,
-        keep_fnames: true,
-      }
-    }
   };
 })();
