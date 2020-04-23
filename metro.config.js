66da1a9effac83f8cf50a8d53bd1e38561bc5769
alk831 <adam54111@gmail.com>
2020-04-23 17:56:34 +0200

    fix: metro minifier migration issue

--- metro.config.js
+++ metro.config.js
@@ -4,6 +4,7 @@ module.exports = (async () => {
   const {
     resolver: { sourceExts, assetExts }
   } = await getDefaultConfig();
+  
   return {
     transformer: {
       getTransformOptions: async () => ({
@@ -17,6 +18,19 @@ module.exports = (async () => {
     resolver: {
       assetExts: assetExts.filter(ext => ext !== "svg"),
       sourceExts: [...sourceExts, "svg"]
+    },
+    minifierPath: 'metro-minify-terser',
+    minifierConfig: {
+      // https://www.npmjs.com/package/terser#mangle-options
+      ecma: 8,
+      keep_classnames: true,
+      keep_fnames: true,
+      module: true,
+      mangle: {
+        module: true,
+        keep_classnames: true,
+        keep_fnames: true,
+      }
     }
   };
 })();
