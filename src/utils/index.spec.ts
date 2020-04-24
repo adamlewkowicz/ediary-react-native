3dcf49454cc3be685bcf8b8c466fffc16a819454
alk831 <adam54111@gmail.com>
2020-04-24 22:49:55 +0200

    add more util specs

--- src/utils/index.spec.ts
+++ src/utils/index.spec.ts
@@ -1,21 +1,17 @@
-import {
-  mapAsyncSequence,
-  filterByUniqueId,
-  findOrFail,
-  sortByMostAccurateName,
-} from '.';
-import { createDebouncedFunc } from './common';
+import * as Utils from './index';
 
 describe('Utils', () => {
 
   describe('createDebouncedFunc', () => {
 
-    test('debounced function gets called only once', () => {
-      jest.useFakeTimers();
+    beforeEach(() => jest.useFakeTimers());
+    afterEach(() => jest.useRealTimers());
+
+    it('should call debounced function only once', () => {
       const actionMock = jest.fn();
     
       const debounceTime = 150;
-      const debouncedAction = createDebouncedFunc(actionMock, debounceTime);
+      const debouncedAction = Utils.createDebouncedFunc(actionMock, debounceTime);
     
       debouncedAction(actionMock, debounceTime);
       debouncedAction(actionMock, debounceTime);
@@ -29,11 +25,11 @@ describe('Utils', () => {
 
   describe('mapAsyncSequence', () => {
 
-    test('runs promises in a sequence', async () => {
+    it('should run promises in a sequence', async () => {
       const promiseMock = jest.fn().mockImplementation(async () => {});
       const dataMock = [1, 2];
     
-      await mapAsyncSequence(dataMock, value => promiseMock(value));
+      await Utils.mapAsyncSequence(dataMock, value => promiseMock(value));
     
       expect(promiseMock).toHaveBeenCalledTimes(dataMock.length);
       expect(promiseMock).toHaveBeenNthCalledWith(1, dataMock[0]);
@@ -44,9 +40,9 @@ describe('Utils', () => {
 
   describe('filterByUniqueId', () => {
 
-    test('filters items by unique id', () => {
+    it('should filter items by unique id', () => {
       const items = [{ id: 1 }, { id: 1 }];
-      const filteredItems = items.filter(filterByUniqueId);
+      const filteredItems = items.filter(Utils.filterByUniqueId);
     
       expect(filteredItems.length).toBeLessThan(items.length);
     });
@@ -55,9 +51,9 @@ describe('Utils', () => {
   
   describe('findOrFail', () => {
   
-    test('throws error if no item was found', () => {
+    it('should throw error if no item was found', () => {
       const items = [{ name: 'A' }];
-      const subject = () => findOrFail(items, item => item.name === 'B');
+      const subject = () => Utils.findOrFail(items, item => item.name === 'B');
       expect(subject).toThrowError();
     });
   
@@ -65,7 +61,7 @@ describe('Utils', () => {
 
   describe('sortByMostAccurateName', () => {
   
-    test('sorts items by most matching name', () => {
+    it('should sort items by most matching name', () => {
       const name = 'orange';
       const dataMock = [
         { name: 'Orange a' },
@@ -73,11 +69,65 @@ describe('Utils', () => {
         { name: 'Orange' },
       ];
     
-      const result = dataMock.sort(sortByMostAccurateName(name));
+      const result = dataMock.sort(Utils.sortByMostAccurateName(name));
     
       expect(result).toMatchSnapshot();
     });
   
   });
+
+  describe('isLastCharEqual', () => {
   
+    it('should return true if last char equals given char', () => {
+      expect(Utils.isLastCharEqual('ab', 'b')).toEqual(true);
+      expect(Utils.isLastCharEqual('ab', 'c')).not.toEqual(true);
+    });
+  
+  });
+
+  describe('isANumber', () => {
+  
+    it('should return true if value is a number', () => {
+      expect(Utils.isANumber(10)).toEqual(true);
+      expect(Utils.isANumber(Number.NaN)).not.toEqual(true);
+    });
+  
+  });
+  
+  describe('isNil', () => {
+  
+    it('should return true if given value is nill (null/undefined)', () => {
+      expect(Utils.isNil(null)).toEqual(true);
+      expect(Utils.isNil(undefined)).toEqual(true);
+      expect(Utils.isNil(5)).not.toEqual(true);
+    });
+  
+  });
+
+  describe('eachValueEqualsZero', () => {
+  
+    it('should return true if each object value equals zero', () => {
+      expect(Utils.eachValueEqualsZero({ a: 0, b: 0 })).toEqual(true);
+      expect(Utils.eachValueEqualsZero({ a: 0, b: 1 })).not.toEqual(true);
+    });
+  
+  });
+
+  describe('cancelablePromise', () => {
+
+    describe('while promise fulfilled', () => {
+    
+      it('should remove listener', async () => {
+        const promiseFake = Promise.resolve();
+        const abortController = new AbortController();
+  
+        await Utils.cancelablePromise(promiseFake, abortController);
+  
+        expect(abortController.signal.removeEventListener).toHaveBeenCalledTimes(1);
+      });
+    
+    });
+  
+  });
+
 });
