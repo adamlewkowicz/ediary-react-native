e7cbf503168aa4bb69b450e8f03040448635604c
alk831 <adam54111@gmail.com>
2020-04-24 17:09:38 +0200

    test: mock svg files

--- jest.config.js
+++ jest.config.js
@@ -15,7 +15,8 @@ module.exports = {
     "<rootDir>/design-system",
   ],
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+    "\\.svg": "<rootDir>/__mocks__/svgMock.js",
   },
   transformIgnorePatterns: [
     "node_modules/(?!(react-native|@testing-library|@react-navigation/.*|react-native-svg|react-native-camera|react-native-screens|react-native-swipe-gestures|react-native-gesture-handler|react-native-safe-area-view|react-native-svg-charts|react-native-iphone-x-helper|@react-native-community|react-native-reanimated)/)"
