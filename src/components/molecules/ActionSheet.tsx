5b55f4924871d9a36fe60498aff266cfc2a2b999
alk831 <adam54111@gmail.com>
2020-04-29 12:14:22 +0200

    update action sheet styling

--- src/components/molecules/ActionSheet.tsx
+++ src/components/molecules/ActionSheet.tsx
@@ -1,5 +1,7 @@
 import React, { useRef, useEffect } from 'react';
-import NativeActionSheet from 'react-native-actionsheet';
+import { ActionSheetCustom as NativeActionSheet } from 'react-native-actionsheet';
+import { StyleSheet } from 'react-native';
+import { THEME } from '../../common/theme';
 
 interface ActionSheetProps<T extends string> {
   title: string
@@ -34,6 +36,59 @@ export const ActionSheet = <T extends string>(props: ActionSheetProps<T>) => {
       options={options}
       cancelButtonIndex={cancelButtonIndex}
       onPress={handleOnPress}
+      styles={style}
     />
   );
 }
+
+const style = StyleSheet.create({
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    opacity: 0.4,
+    backgroundColor: '#000'
+  },
+  wrapper: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  body: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    backgroundColor: THEME.color.tertiary,
+  },
+  titleBox: {
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff'
+  },
+  titleText: {
+    color: THEME.color.primary,
+    fontFamily: THEME.fontWeight.light,
+    fontSize: THEME.fontSizeRaw.regular,
+  },
+  messageBox: {
+    height: 40,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: THEME.color.primaryLight
+  },
+  buttonBox: {
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: THEME.color.primaryLight
+  },
+  buttonText: {
+    fontSize: THEME.fontSizeRaw.regular,
+    fontFamily: THEME.fontWeight.regular,
+    color: THEME.color.highlight,
+  },
+});
